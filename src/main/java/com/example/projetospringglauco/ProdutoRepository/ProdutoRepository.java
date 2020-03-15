faf3302bab5d5a924eb52f306afca3c4b8a9df81
Leonardo Sanches <sleonardoGabriel@gmail.com>
2020-03-15 18:21:15 -0300

    Ajustes

--- src/main/java/com/example/projetospringglauco/ProdutoRepository/ProdutoRepository.java
+++ src/main/java/com/example/projetospringglauco/ProdutoRepository/ProdutoRepository.java
@@ -17,16 +17,16 @@ public class ProdutoRepository {
 
     public ProdutoRepository()
     {
-        produtos.put(1, new produtoModel(1, "CALABRESA", 10.00, 50));
-        produtos.put(2, new produtoModel(2, "BACON", 8.00, 20));
-        produtos.put(3, new produtoModel(3, "FRANGO", 5.00, 30));
-        produtos.put(4, new produtoModel(4, "QUEIJO", 25.00, 50));
-        produtos.put(5, new produtoModel(5, "PRESUNTO", 12.00, 0));
-        produtos.put(6, new produtoModel(6, "PAO", 4.00, 20));
-        produtos.put(7, new produtoModel(7, "SALAME", 15.00, 75));
-        produtos.put(8, new produtoModel(8, "GORGONZOLA", 20.00, 20));
-        produtos.put(9, new produtoModel(9, "AMENDOIM", 10.00, 80));
-        produtos.put(10, new produtoModel(10, "PEITO DE PERU", 20.00, 10));
+        produtos.put(1, new produtoModel(1, "Calafrango", 10.00, 50));
+        produtos.put(2, new produtoModel(2, "X-Bacon", 8.00, 20));
+        produtos.put(3, new produtoModel(3, "Frango", 5.00, 30));
+        produtos.put(4, new produtoModel(4, "Franqueijo", 25.00, 50));
+        produtos.put(5, new produtoModel(5, "Misto", 12.00, 0));
+        produtos.put(6, new produtoModel(6, "X-Burguer", 4.00, 20));
+        produtos.put(7, new produtoModel(7, "Mineirinho", 15.00, 75));
+        produtos.put(8, new produtoModel(8, "Cerveja corona", 20.00, 20));
+        produtos.put(9, new produtoModel(9, "Coca-cola", 10.00, 80));
+        produtos.put(10, new produtoModel(10, "Porção de fritas", 20.00, 10));
     }
 
     public produtoModel getProdutoById(int id) {
